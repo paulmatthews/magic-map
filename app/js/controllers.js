964825edceed16e5ba71caa3bfa977a7123d940a
Paul Matthews <matthewspaulc@gmail.com>
2014-02-23 17:56:04 -0500

    Add remove place functionality. Remove unused MyCtrl2.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,15 +5,23 @@
 angular.module('myApp.controllers', []).
   controller('MyCtrl1', ['$scope', 'Map', function($scope, Map) {
     $scope.map = Map;
-    $scope.addPlace = function() {
+    
+    /* Add new place to map.locs */
+    $scope.places = function(mode) {
       var place = {},
           latlong = $scope.newloc.split(',');
       place.latitude = latlong[0];
       place.longitude = latlong[1];
-      console.log($scope.map.locs);
-      $scope.map.locs.push(place);
-    }
-  }])
-  .controller('MyCtrl2', [function() {
-
+      mode.localeCompare('add') === 0 ? $scope.map.locs.push(place) :
+                                        $scope.removePlace(place);
+    };
+    /* Remove place */
+    $scope.removePlace = function(place) {
+        for (var i=0;i<$scope.map.locs.length;i++) {
+            if(place.latitude.localeCompare($scope.map.locs[i].latitude) === 0 &&
+                place.longitude.localeCompare($scope.map.locs[i].longitude) === 0) {
+                    $scope.map.locs.splice(i, 1);
+            }
+        }
+    };
   }]);
